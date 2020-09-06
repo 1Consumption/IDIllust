e65219674308de2ef97d3739d23a32b7de422509
1Consumption <hanseop95@gmail.com>
2020-09-06 21:14:16 +0900

    refactor: BE 합류로 인한 디렉토리 변경

--- iOS/IDIllust/IDIllust/CustomizeView/CustomizeViewController.swift
+++ iOS/IDIllust/IDIllust/CustomizeView/CustomizeViewController.swift
@@ -2,14 +2,14 @@
 //  CustomizeViewController.swift
 //  IDIllust
 //
-//  Created by 신한섭 on 2020/09/04.
+//  Created by 신한섭 on 2020/09/06.
 //  Copyright © 2020 신한섭. All rights reserved.
 //
 
 import UIKit
 
 final class CustomizeViewController: UIViewController {
-    
+
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
