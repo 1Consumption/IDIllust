9cef559a06c24c321d299e760b2b3dca9e205cce
sedin <leesejinn@naver.com>
2020-09-09 20:03:40 +0900

    feat: spring boot 초기 세팅
    spring boot

---
+++ BE/src/main/java/com/id/illust/IllustApplication.java
@@ -0,0 +1,13 @@
+package com.id.illust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IllustApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/id/illust/IllustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.id.illust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
