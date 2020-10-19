61f79c385f3709e492c6b38f2c652307fe534924
1Consumption <hanseop95@gmail.com>
2020-10-19 15:20:19 +0900

    feat: EndPoint 구조체 생성
    - urlComponent들을 조합해 URL로 반환해주는 구조체
    
    #8

---
+++ iOS/IDIllust/IDIllust/Network/EndPoint.swift
@@ -0,0 +1,40 @@
+//
+//  EndPoint.swift
+//  IDIllust
+//
+//  Created by 신한섭 on 2020/10/19.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+struct EndPoint {
+    
+    init(path: Path) {
+        self.path = path
+    }
+    
+    var url: URL? {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = host
+        components.path = path.description
+        
+        return components.url
+    }
+    
+    private let scheme = "http"
+    private let host = "3.34.77.7"
+    private let path: Path
+    
+    enum Path {
+        case entry
+        
+        var description: String {
+            switch self {
+            case .entry:
+                return "/api/entry"
+            }
+        }
+    }
+}
