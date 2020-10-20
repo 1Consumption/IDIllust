58a6113b6623bbfa83d318d476e1ef2aaa4e76e8
1Consumption <hanseop95@gmail.com>
2020-10-20 23:04:31 +0900

    feat: EntryViewUseCase 연동
    - retrieveEntryImageInfo 메소드를 호출하여 서버에서 모델을 받아옴.
    
    #10

--- iOS/IDIllust/IDIllust/EntryView/EntryViewController.swift
+++ iOS/IDIllust/IDIllust/EntryView/EntryViewController.swift
@@ -6,12 +6,32 @@
 //  Copyright © 2020 신한섭. All rights reserved.
 //
 
+import Kingfisher
 import UIKit
 
 final class EntryViewController: UIViewController {
-
+    
+    // MARK: - @IBOutlet
+    @IBOutlet weak var entryImageView: UIImageView!
+    
+    // MARK: - Properties
+    private var entryImage: EntryImage?
+    
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        entryImageUseCase()
+    }
+    
+    // MARK: - Methods
+    private func entryImageUseCase() {
+        EntryImageUseCase()
+            .retrieveEntryImageInfo(networkManager: NetworkManager(),
+                                    failureHandler: { _ in
+                                        // Todo: UseCaseError에 따른 예외 처리
+                                    },
+                                    successHandler: { [weak self] in
+                                        self?.entryImage = $0
+                                    })
     }
 }
