461282f7ee703642a266601d8d9ca679afe2a3e0
1Consumption <hanseop95@gmail.com>
2020-10-23 19:32:17 +0900

    feat: RetrieveModelFromServer 프로토콜 구현
    - useCase의 중복되는 동작(서버에서 모델을 받아오는 것)을 줄이기 위해 프로토콜 기본 구현으로 기능을 제공 함
    
    #16

---
+++ iOS/IDIllust/IDIllust/Protocols/RetrieveModelFromServer.swift
@@ -0,0 +1,42 @@
+//
+//  RetrieveModelFromServer.swift
+//  IDIllust
+//
+//  Created by 신한섭 on 2020/10/23.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+protocol RetrieveModelFromServer {
+    associatedtype Model
+    
+    func retrieveModel(from endPoint: EndPoint, networkManager: NetworkManageable, failurehandler: @escaping (UseCaseError) -> Void, successHandler: @escaping (Model) -> Void) -> URLSessionDataTask?
+}
+
+extension RetrieveModelFromServer where Model: Codable {
+    @discardableResult
+    func retrieveModel(from endPoint: EndPoint, networkManager: NetworkManageable, failurehandler: @escaping (UseCaseError) -> Void, successHandler: @escaping (Model) -> Void) -> URLSessionDataTask? {
+        let url = endPoint.url
+        
+        let task = networkManager.getResource(url: url,
+                                  method: .get,
+                                  headers: nil,
+                                  handler: { result in
+                                    switch result {
+                                    case .success(let data):
+                                        do {
+                                            let model = try JSONDecoder().decode(Model.self, from: data)
+                                            successHandler(model)
+                                        } catch {
+                                            failurehandler(.decodeError)
+                                        }
+                                        
+                                    case .failure(let error):
+                                        failurehandler(.networkError(error))
+                                    }
+                                  })
+        
+        return task
+    }
+}
