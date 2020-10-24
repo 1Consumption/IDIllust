087584b38352f57b114974402b78c948d39ed567
1Consumption <hanseop95@gmail.com>
2020-10-24 15:24:06 +0900

    feat: Components UseCase 테스트 작성
    - 유닛 테스트 작성
    
    #12

---
+++ iOS/IDIllust/IDIllustTests/UseCase/ComponentsUseCaseTests.swift
@@ -0,0 +1,31 @@
+//
+//  ComponentsUseCaseTests.swift
+//  IDIllustTests
+//
+//  Created by 신한섭 on 2020/10/24.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+@testable import IDIllust
+import XCTest
+
+class ComponentsUseCaseTests: XCTestCase {
+    
+    private var mockNetworkManager: NetworkManageable!
+    private var model: Components!
+
+    override func setUpWithError() throws {
+        let component = IDIllust.Component(id: 1, name: "test", url: "url")
+        model = Components(components: [component])
+    }
+    
+    func testSuccess() {
+        mockNetworkManager = MockSuccessNetworkManager(model: model)
+        
+        ComponentsUseCase().retrieveComponents(networkManager: mockNetworkManager,
+                                               categoryId: 0,
+                                               successHandler: { model in
+                                                XCTAssertEqual(self.model, model)
+                                               })
+    }
+}
