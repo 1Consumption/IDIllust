3d6cde27a5eac651a2951baa606604d8543237cc
sedin <leesejinn@naver.com>
2020-10-25 23:23:22 +0900

    feat: Component만 click시 controller 추가
    - categoryId, componentId로만 요청시 default image 내려줌

--- BE/src/main/java/com/id/illust/controller/EntryController.java
+++ BE/src/main/java/com/id/illust/controller/EntryController.java
@@ -3,13 +3,11 @@ package com.id.illust.controller;
 import com.id.illust.network.request.EntryApiRequest;
 import com.id.illust.network.response.EntryApiResponse;
 import com.id.illust.service.EntryService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 public class EntryController {
 
@@ -21,7 +19,6 @@ public class EntryController {
 
     @PostMapping("/entry")
     public EntryApiResponse createEntry(@RequestBody EntryApiRequest entryApiRequest) {
-        log.info("{}", entryApiRequest);
         return entryService.create(entryApiRequest);
     }
 
--- BE/src/main/java/com/id/illust/service/ComponentService.java
+++ BE/src/main/java/com/id/illust/service/ComponentService.java
@@ -1,7 +1,8 @@
 package com.id.illust.service;
 
-import com.id.illust.entity.Category;
+import com.id.illust.entity.Color;
 import com.id.illust.entity.Component;
+import com.id.illust.network.response.ChoiceApiResponse;
 import com.id.illust.network.response.ComponentApiResponse;
 import com.id.illust.repository.CategoryRepository;
 import com.id.illust.repository.ComponentRepository;
@@ -28,9 +29,22 @@ public class ComponentService {
                                  .orElseGet(() -> response(new ArrayList<>()));
     }
 
+    public ChoiceApiResponse readFirst(Long categoryId, Long componentId) {
+        return componentRepository.findById(componentId)
+                                  .filter(component -> component.getCategory().getId().equals(categoryId))
+                                  .map(component -> response(component.getColors().get(0)))
+                                  .orElseGet(() -> response(new Color()));
+    }
+
     private ComponentApiResponse response(List<Component> componentList) {
         return ComponentApiResponse.builder()
                                    .components(componentList)
                                    .build();
+    }
+
+    private ChoiceApiResponse response(Color color) {
+        return ChoiceApiResponse.builder()
+                                .thumbUrl(color.getThumbUrl())
+                                .build();
     }
 }
