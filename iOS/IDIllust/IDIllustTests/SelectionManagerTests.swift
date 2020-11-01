a6b3e2561c2c00bfaa43e82b89d7ebf14c7fef15
1Consumption <hanseop95@gmail.com>
2020-11-01 22:02:20 +0900

    feat: selectManager에 추가된 메소드 유닛테스트
    - isSelectedComponent
    - removeCurrentComponent
    
    #39

--- iOS/IDIllust/IDIllustTests/SelectionManagerTests.swift
+++ iOS/IDIllust/IDIllustTests/SelectionManagerTests.swift
@@ -13,6 +13,8 @@ class SelectionManagerTests: XCTestCase {
     
     private var selectionManager: SelectionManager!
     private var currentSelection: CurrentSelection!
+    private let categoryId = 0
+    private let componentId = 1
     
     override func setUpWithError() throws {
         selectionManager = SelectionManager()
@@ -20,26 +22,44 @@ class SelectionManagerTests: XCTestCase {
     }
     
     func testSetCurrentCategoryIdCategoryIndex() {
-        currentSelection.categoryId = 0
+        currentSelection.categoryId = categoryId
         currentSelection.categoryIndex = 0
-        
-        selectionManager.setCurrent(categoryId: 0, categoryIndex: 0)
+        selectionManager.setCurrent(categoryId: categoryId, categoryIndex: 0)
         
         XCTAssertEqual(selectionManager.current, currentSelection)
     }
     
     func testSetCurrentComponentId() {
-        currentSelection.componentId = 1
-        
-        selectionManager.setCurrent(componentId: 1)
+        currentSelection.componentId = componentId
+        selectionManager.setCurrent(componentId: componentId)
         
         XCTAssertEqual(selectionManager.current, currentSelection)
     }
     
     func testSelectionValid() {
-        selectionManager.setCurrent(categoryId: 1, categoryIndex: 0)
-        selectionManager.setCurrent(componentId: 2)
+        selectionManager.setCurrent(categoryId: categoryId, categoryIndex: 0)
+        selectionManager.setCurrent(componentId: componentId)
         
-        XCTAssertEqual(selectionManager.selection, [1: 2])
+        XCTAssertEqual(selectionManager.selection, [categoryId: componentId])
+    }
+    
+    func testIsSelectedComponent() {
+        selectionManager.setCurrent(categoryId: categoryId, categoryIndex: 0)
+        selectionManager.setCurrent(componentId: componentId)
+        
+        XCTAssertTrue(selectionManager.isSelectedComponent(categoryId: categoryId, componentId: componentId))
+        XCTAssertFalse(selectionManager.isSelectedComponent(categoryId: categoryId, componentId: 9))
+    }
+    
+    func testRemoveCurrentComponent() {
+        XCTAssertNil(selectionManager.removeCurrentComponent())
+        
+        selectionManager.setCurrent(categoryId: categoryId, categoryIndex: 0)
+        selectionManager.setCurrent(componentId: componentId)
+        
+        XCTAssertEqual(selectionManager.removeCurrentComponent(), componentId)
+        
+        XCTAssertNil(selectionManager.current.componentId)
+        XCTAssertNil(selectionManager.selection[categoryId])
     }
 }
