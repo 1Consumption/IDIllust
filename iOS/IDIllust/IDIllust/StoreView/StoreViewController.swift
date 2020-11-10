6baef32fcebd2f48333930341d9ee5f8cdb10573
1Consumption <hanseop95@gmail.com>
2020-11-10 20:54:31 +0900

    feat: textField 입력 제한
    - ViewController에서 UITextFieldDelegate프로토콜을 채택하고 설정된 limit을 넘어 입력을 받지 않도록 설정
    - textLength: 현재 textField의 텍스트 길이, string: 새로 추가될 텍스트, range: 대체되는 범위
    
    #59

--- iOS/IDIllust/IDIllust/StoreView/StoreViewController.swift
+++ iOS/IDIllust/IDIllust/StoreView/StoreViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 final class StoreViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    private let limit: Int = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.systemBackground.withAlphaComponent(0)
+        titleTextField.delegate = self
+    }
+}
+extension StoreViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let textLength = textField.text?.count else { return true }
+        let length = textLength + string.count - range.length
+        
+        return length <= limit
     }
 }
