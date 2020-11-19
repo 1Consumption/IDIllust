766be548c7d7613336100a31fea4508961279847
1Consumption <hanseop95@gmail.com>
2020-11-19 18:47:52 +0900

    chore: Localization resource 생성
    #78

---
+++ iOS/IDIllust/IDIllust/Localization/Localization.swift
@@ -0,0 +1,34 @@
+//
+//  Localization.swift
+//  IDIllust
+//
+//  Created by 신한섭 on 2020/11/19.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+struct Localization {
+    static let tutorialImageName = (1...6).map { String($0).localized() }
+    static let skip = "skip".localized()
+    static let start = "start".localized()
+    static let imageLoadingFailureAlertTitle = "imageLoadingFailureAlertTitle".localized()
+    static let imageLoadingFailureAlertMessage = "imageLoadingFailureAlertMessage".localized()
+    static let shouldSelectImageAlertMessage = "shouldSelectImageAlertMessage".localized()
+    static let saveSuccessAlertTitle = "saveSuccessAlertTitle".localized()
+    static let saveSuccessAlertMessage = "saveSuccessAlertMessage".localized()
+    static let furtherEditing = "furtherEditing".localized()
+    static let createNew = "createNew".localized()
+    static let confirm = "confirm".localized()
+    static let cancel = "cancel".localized()
+    static let settingUp = "settingUp".localized()
+    static let previousItemEditAlertMessage = "previousItemEditAlertMessage".localized()
+    static let requestForPermissionAlertTitle = "requestForPermissionAlertTitle".localized()
+    static let requestForPermissionAlertMessage = "requestForPermissionAlertMessage".localized()
+}
+
+extension String {
+    func localized(bundle: Bundle = .main, tableName: String = "Localizable") -> String {
+        return NSLocalizedString(self, tableName: tableName, value: "**\(self)**", comment: "")
+    }
+}
